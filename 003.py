a1d9114564e1207c3cb92ed2d49fd2b868616d9e
Zachary Suarez <40529691+suarzac@users.noreply.github.com>
2022-01-21 12:53:17 -0800

    003 solution

--- 003.py
+++ 003.py
@@ -6,11 +6,24 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143?
 """
+def largest_fact():
+    max_prime = 0
+    thresh = num
 
-prime_start, to_factor = 2, 600851475143
-while (prime_start*prime_start <= to_factor):
-	if (to_factor%prime_start == 0):
-		to_factor /= prime_start
-	else:
-		prime_start += 2
-print(to_factor)
+    for i in range(1,(num+1)):
+        if (i>thresh):
+            break
+        if is_factor(i) and is_prime(i):
+            max_prime = i
+            thresh = num/i
+    return max_prime
+def is_factor(i):
+    return num%i == 0
+def is_prime(i):
+    count = 0
+    for j in range(1,i+1):
+        if i%j == 0:
+            count += 1
+    return count == 2
+num = 600851475143
+print(largest_fact())
